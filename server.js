401dde9b37bd5dd50ac947dac350b2cab87155df
Woon <c1vian00@students.oamk.fi>
2025-02-06 21:40:54 +0200

    All assignments done and tests succeed

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.use(express.json()); // for parsing application/json
 
 // ------ WRITE YOUR SOLUTION HERE BELOW ------//
 
+const levelHighScores = []
+
 //SIGNUP LOGIC
 app.post("/signup", (req, res) => {
   const { userHandle, password } = req.body
@@ -41,7 +43,46 @@ app.post("/login", (req, res) => {
     return res.status(400).send()
   }
 
-  res.status(200).json({jsonWebToken: "string"})
+  res.status(200).json({jsonWebToken: "randomString"})
+})
+
+//HIGHSCORE LOGIC
+function authMiddlewear(req, res, next) {
+  const { authorization }  = req.headers
+
+  if (!authorization) {
+    return res.status(401).send()
+  }
+
+  if ( authorization != "Bearer randomString") {
+    return res.status(401).send()
+  }
+
+  next()
+}
+
+app.post("/high-scores", authMiddlewear, (req, res) => {
+  const { level, userHandle, score, timestamp } = req.body
+
+  if (!level || !userHandle || !score || !timestamp) {
+    return res.status(400).send()
+  }
+  const item = { level, userHandle, score, timestamp }
+  levelHighScores.push(item)
+
+  res.status(201).send()
+})
+
+app.get("/high-scores", (req, res) => {
+  const { level, page = 1 } = req.query
+  
+  const allResult = levelHighScores.filter((item) => level == item.level)
+
+  allResult.sort((a, b) => b.score - a.score)
+
+  const pageResult = allResult.slice((page-1)*20, page*20)
+
+  res.status(200).json(pageResult)
 })
 
 //------ WRITE YOUR SOLUTION ABOVE THIS LINE ------//
